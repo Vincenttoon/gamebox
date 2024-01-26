ec8fe54423bb79bf9f865e804e7d89ebe6ecff46
Vincent Toon <vincenttoon22@gmail.com>
2024-01-25 23:25:00 -0600

    error gone away, formatting specificity in vue?

--- views/Home.vue
+++ views/Home.vue
@@ -1,1 +1,16 @@
-<template>
+// <template>
+   <v-container>
+   <h1>Hello </h1>
+   </v-container>
+// </template>
+
+<script>
+export default {
+  name: 'home-component', // Use kebab-case for the component name
+  // Your component code here
+}
+</script>
+
+<style>
+/* Your component styles here */
+</style>~
